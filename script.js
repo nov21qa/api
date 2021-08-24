389b40c71fad33bff493d512f4ea19b5e6ef7197
nov21qa <86826333+nov21qa@users.noreply.github.com>
2021-08-24 17:55:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const request = new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3/name/portugal');
+request.send();
+
+request.addEventListener('load',function()
+{
+    console.log(request.responseText);
+}
+);
