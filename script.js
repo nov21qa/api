7516330807ba063076ca8894ad6e197ce74ea11b
nov21qa <86826333+nov21qa@users.noreply.github.com>
2021-08-24 18:08:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 const request = new XMLHttpRequest();
-request.open('GET','https://restcountries.com/v3/name/portugal');
+request.open('GET','https://restcountries.com/v3/name/india');
 request.send();
 
 request.addEventListener('load',function()
